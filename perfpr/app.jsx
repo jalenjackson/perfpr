ed1f09cc56adbcd7b4cdd843f1ec553361839be4
Jalen Jackson <jalenjack201@hotmail.com>
2017-08-31 17:36:05 -0400

    Added tracker react to output inputted data from the database

--- perfpr/app.jsx
+++ perfpr/app.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import TrackerReact from 'meteor/ultimatejs:tracker-react'
 
 PerfectPresence = new Mongo.Collection("perfect-presence");
 
-export default class App extends Component {
+export default class App extends TrackerReact(Component) {
+
+    perfectPresence(){
+        return PerfectPresence.find().fetch();
+    }
 
     addPerfectPresence(event){
         event.preventDefault();
@@ -20,12 +25,27 @@ export default class App extends Component {
     }
 
     render(){
+        console.log(this.perfectPresence());
+        let perfectPresenceTable = this.perfectPresence();
+
+        if(perfectPresenceTable.length < 1) {
+            return (<div>Loading...</div>)
+        }
+
         return (
             <div>
                 <h1>Perfect Presence</h1>
                 <form className="perfect-presence" onSubmit={this.addPerfectPresence.bind(this)}>
                     <input type="text" ref="perfectPresence" placeholder="Test" />
                 </form>
+
+                <div>
+                    {perfectPresenceTable.map((perf)=>{
+                       return (
+                           perf.text 
+                       )
+                    })}
+                </div>
             </div>
         )
     }
