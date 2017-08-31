31ad357f80dbb510960a2e81ee58bdb133f6e271
Jalen Jackson <jalenjack201@hotmail.com>
2017-08-31 17:11:46 -0400

    Added meteor toys and Set up React to add records to the MongoDB database

--- perfpr/app.jsx
+++ perfpr/app.jsx
@@ -1,11 +1,31 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+PerfectPresence = new Mongo.Collection("perfect-presence");
+
 export default class App extends Component {
+
+    addPerfectPresence(event){
+        event.preventDefault();
+        var text = this.refs.perfectPresence.value.trim();
+
+        PerfectPresence.insert({
+            text: text,
+            complete: false,
+            createdAt: new Date()
+        });
+
+        this.refs.perfectPresence.value = "";
+
+    }
+
     render(){
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>Perfect Presence</h1>
+                <form className="perfect-presence" onSubmit={this.addPerfectPresence.bind(this)}>
+                    <input type="text" ref="perfectPresence" placeholder="Test" />
+                </form>
             </div>
         )
     }
