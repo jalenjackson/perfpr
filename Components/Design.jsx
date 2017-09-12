bf0cb274b67c78af1b88f22ed46053839aa6ea65
Jalen Jackson <jalenjack201@hotmail.com>
2017-09-12 00:22:01 -0400

    did design page

--- Components/Design.jsx
+++ Components/Design.jsx
@@ -1,161 +1,84 @@
 import React, {Component} from 'react'
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import Footer from "./Footer";
+
 
 export default class Design extends Component {
 
-    componentDidMount(){
-        var isAtLocation = false;
-
-        if(window.location.pathname === "/design"){
-            isAtLocation = true;
-        }
-
-        if(isAtLocation) {
-
-            var cvs = document.createElement('canvas'),
-                context = cvs.getContext("2d");
-            document.getElementById("comingsoon").appendChild(cvs);
-
-            var numDots = 200,
-                n = numDots,
-                currDot,
-                maxRad = 800,
-                minRad = 100,
-                radDiff = maxRad - minRad,
-                dots = [],
-                pairs = [];
-            PI = Math.PI,
-                centerPt = {x: 0, y: 0};
-
-            resizeHandler();
-            window.onresize = resizeHandler;
-
-// create dots
-            n = numDots
-            while (n--) {
-                currDot = {};
-                currDot.x = currDot.y = 0;
-                currDot.radius = minRad + Math.random() * radDiff;
-                currDot.radiusV = 10 + Math.random() * 50,
-                    currDot.radiusVS = (1 - Math.random() * 2) * 0.015,
-                    currDot.radiusVP = Math.random() * PI,
-                    currDot.ang = (1 - Math.random() * 2) * PI;
-                currDot.speed = 0;
-                currDot.intensity = Math.round(Math.random() * 255);
-                currDot.fillColor = "rgb(" + currDot.intensity + "," + currDot.intensity + "," + currDot.intensity + ")";
-                dots.push(currDot);
-            }
-
-//create all pairs
-
-            n = numDots
-            while (n--) {
-                ni = n;
-                while (ni--) {
-                    pairs.push([n, ni]);
-                }
-            }
-
-            function drawPoints() {
-                n = numDots;
-                var _centerPt = centerPt,
-                    _context = context,
-                    dX = 0,
-                    dY = 0;
-
-                _context.clearRect(0, 0, cvs.width, cvs.height);
-
-                var radDiff;
-                //move dots
-                n = numDots;
-                while (n--) {
-                    currDot = dots[n];
-                    currDot.radiusVP += currDot.radiusVS;
-                    radDiff = currDot.radius + Math.sin(currDot.radiusVP) * currDot.radiusV;
-                    currDot.x = _centerPt.x + Math.sin(currDot.ang) * radDiff;
-                    currDot.y = _centerPt.y + Math.cos(currDot.ang) * radDiff;
-
-                    //currDot.ang += currDot.speed;
-                    currDot.ang += currDot.speed * radDiff / 210000;
-                }
-
-                var pair, dot0, dot1, dist, bright,
-                    maxDist = Math.pow(100, 2);
-                //draw lines
-                n = pairs.length;
-                while (n--) {
-                    pair = pairs[n];
-                    dot0 = dots[pair[0]];
-                    dot1 = dots[pair[1]];
-                    dist = Math.pow((dot1.x - dot0.x), 2) + Math.pow((dot1.y - dot0.y), 2);
-                    if (dist < maxDist) {
-                        bright = 22;
-                        _context.beginPath();
-                        _context.moveTo(dot0.x, dot0.y);
-                        _context.lineTo(dot1.x, dot1.y);
-                        _context.lineWidth = 1;
-                        _context.strokeStyle = "rgb(" + bright + "," + bright + "," + bright + ")";
-                        _context.stroke();
-                    }
-                }
-
-                //draw dots
-                n = numDots;
-                while (n--) {
-                    //console.log(currDot);
-                    _context.fillStyle = dots[n].fillColor;
-                    _context.fillRect(dots[n].x, dots[n].y, 1, 1);
-                }
-                window.requestAnimationFrame(drawPoints);
-            }
-
-            function resizeHandler() {
-                var box = cvs.getBoundingClientRect();
-                var w = box.width;
-                var h = box.height;
-                cvs.width = w;
-                cvs.height = h;
-                centerPt.x = Math.round(w / 2);
-                centerPt.y = Math.round(h / 2);
-            }
-
-            drawPoints();
-
-        }
-    }
 
 
     render(){
         return(
-            <div id="design-div">
-
-                <div id="comingsoon">
-                    <div className="coming-soon-text">
-                        <h1 className="slide-up">Coming Soon</h1>
-                        <p style={{opacity: 0, animationDelay: "0.3s"}} className="slide-up"> Sign up to stay updated</p>
-                    </div>
-
-                </div>
+            <div id="design-page">
 
 
-                <div style={{zIndex: -1}} className="quick-links">
-                        <div style={{background: "rgb(21,21,21)"}} className="quick-link-img">
-                            <h4>Design</h4>
-                        </div>
-                        <div style={{background: "rgb(21,21,21)"}} className="quick-link-img">
-                            <h4>Strategy</h4>
-                        </div>
-                        <div style={{background: "rgb(21,21,21)"}} className="quick-link-img">
-                            <h4>Development</h4>
-                        </div>
 
-                    </div>
 
-                <div style={{display: "none"}} className="design-header">
+
+
+                <div className="design-header">
+
+
+
+                    <ReactCSSTransitionGroup
+                        component="div"
+                        transitionName="change"
+                        transitionEnterTimeout={6000}
+                        transitionAppearTimeout={2000}
+                        transitionLeaveTimeout={4000}
+                        transitionAppear={true}
+                    >
                     <div className="design-info">
-                        <h1>Visual Design</h1>
+                        <h1>Visual <span style={{color: "rgb(118,182,83)"}}>Design</span></h1>
                         <p>Tie It All Together And Put a Nice Bow On It!</p>
                     </div>
+                    </ReactCSSTransitionGroup>
+
+
+
+
+                    <div className="pin-container">
+                        <div className="pin move-pin">
+
+                            <div className="design-img-div design-div-1">
+                            </div>
+                            <h1>UI/UX Design</h1>
+                            <p>Built for Users</p>
+                        </div>
+                        <div style={{animationDelay: "0.2s"}} className="pin move-pin">
+                            <div className="design-img-div design-div-2">
+                            </div>
+                            <h1>Branding</h1>
+                            <p>A new look & feel</p>
+                        </div>
+                        <div style={{animationDelay: "0.4s"}} className="pin move-pin">
+                            <div className="design-img-div design-div-3">
+                            </div>
+
+                            <h1>Media</h1>
+                            <p>Create your media</p>
+                        </div>
+                        <div style={{animationDelay: "0.6s"}} className="pin move-pin">
+                            <div className="design-img-div design-div-4">
+                            </div>
+                            <h1>Prototyping</h1>
+                            <p>Test your idea visually</p>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="design-message">
+                    <div className="circle-div"></div>
+                    <p className="parallax-scroll ">Let your <span style={{color: "rgb(118,182,83)"}}> creativity </span> prosper</p>
+                    <button className="button-consultation design-button" >Start Today</button>
+
+                </div>
+
+
+                <Footer/>
+
+
+                
             </div>
 
         )
--- client/getStarted.js
+++ client/getStarted.js
@@ -3,10 +3,31 @@ $(window).scroll(function(){
 
     console.log(wScroll);
 
+
+
     if(wScroll > 0){
         $(".parallax-scroll").css({
             "transform":'translate(0px, ' + wScroll/15 + '%)'
         });
+
     }
+
+    if(wScroll > 20){
+        $(".design-header").css({
+            "backgroundPosition":'250px -600px'
+        });
+    } else {
+        $(".design-header").css({
+            "backgroundPosition":'350px -770px'
+        });
+    }
+
+
+
+
+
+
+
+
 });
 
